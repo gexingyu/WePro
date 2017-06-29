9e9944bf069a16ef59a322f4232b3baf21592f0e
gexingyu <1054182577@qq.com>
2017-06-29 23:44:46 +0800

    jia zhu shi

--- WeChatMenuProject/src/main/java/com/wechat/app/Application.java
+++ WeChatMenuProject/src/main/java/com/wechat/app/Application.java
@@ -8,7 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //@PropertySources({ @PropertySource("classpath:application.properties")/*,@PropertySource("classpath:spring-mvc.xml")*/ })
 
 public class Application {
-
+/**
+ * 启动器
+ * 
+ * @param args
+ */
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
